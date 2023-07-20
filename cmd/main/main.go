5a9773592d27904c7a423634bb906ea399813ae9
Василий Смоляр <smolyar4@gmail.com>
2023-07-20 19:33:19 +0300

    feat: task CRUD router

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"ex-server/internal/handlers"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/Tasks", handlers.GetTasksList).Methods("GET")
+	r.HandleFunc("/Tasks/{id}", handlers.GetTask).Methods("GET")
+	r.HandleFunc("/Tasks", handlers.CreateTask).Methods("POST")
+	r.HandleFunc("/Tasks/{id}", handlers.UpdateTask).Methods("PUT")
+	r.HandleFunc("/Tasks/{id}", handlers.DeleteTask).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ex-server
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0
