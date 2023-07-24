6e8e9abdc812cd853a4812385c9e14f08b22ee80
Василий Смоляр <smolyar4@gmail.com>
2023-07-25 01:20:00 +0300

    feat: postgres support

---
+++ pkg/db/postgres.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"ex-server/pkg/config"
+	"ex-server/pkg/env"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+
+	"gorm.io/gorm"
+)
+
+func NewConnection(config config.Config, entities ...interface{}) (*gorm.DB, error) {
+	conn, err := connect(config)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = migrate(conn, entities); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func connect(config config.Config) (*gorm.DB, error) {
+	host := env.GetHost(config.GetString("DB.Host"), "db")
+
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s",
+		host,
+		config.GetString("DB.User"),
+		config.GetString("DB.Name"),
+		config.GetString("DB.SSLMode"),
+		config.GetString("DB.Pass"),
+	)
+	log.Println("Connecting to DB: ", dsn)
+
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
+
+func migrate(conn *gorm.DB, entities []interface{}) error {
+	return conn.AutoMigrate(entities...)
+}
