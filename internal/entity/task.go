72fb505c96983358a82dbc0e4e7441d566682323
Василий Смоляр <smolyar4@gmail.com>
2023-07-25 01:21:03 +0300

    fix: refactor models to gorm format

--- internal/entity/task.go
+++ internal/entity/task.go
@@ -1,13 +1,19 @@
 package entity
 
+import "gorm.io/gorm"
+
 type Task struct {
-	ID       string `json:"id"`
-	Title    string `json:"title"`
-	Desc     string `json:"desc"`
-	Assigned *User  `json:"user"`
+	gorm.Model
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Desc   string `json:"desc"`
+	UserID int
+	User   *User `json:"user"`
 }
 
 type User struct {
+	gorm.Model
+	ID        string `json:"id"`
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
 }
