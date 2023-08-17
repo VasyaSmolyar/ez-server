e181e804ddd4cae3f2623d618b04dbc28ef05679
Василий Смоляр <smolyar4@gmail.com>
2023-08-18 01:04:09 +0300

    feat: download handler

--- internal/object/handler/download_file.go
+++ internal/object/handler/download_file.go
@@ -1,7 +1,20 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+)
 
 func (h *Handler) DownloadFile(w http.ResponseWriter, r *http.Request) {
+	filename := r.URL.Query().Get("filename")
 
+	w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote(filename))
+	w.Header().Set("Content-Type", "application/octet-stream")
+
+	err := h.ObjectRepo.DownloadFile(r.Context(), filename, filename)
+	if err != nil {
+		HandleError(err, w)
+		return
+	}
+	http.ServeFile(w, r, filename)
 }
